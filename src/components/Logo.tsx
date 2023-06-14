8c347b4ada79c94fb67c7396876d3864d41046e7
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-14 15:20:34 -0300

    components

---
+++ src/components/Logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='text-4xl'>
+      <span className='font-bold uppercase text-white'>
+        iron
+        <span className='text-red'>gym</span>
+      </span>
+    </div>
+  )
+}
+
+export default Logo
---
+++ src/components/NavList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavList = () => {
+  return (
+    <ul className='uppercase text-white flex items-center gap-10'>
+      <li>Home</li>
+      <li>Service</li>
+      <li>Trainers</li>
+      <li>Testimonial</li>
+      <li>Contact Us</li>
+    </ul>
+  )
+}
+
+export default NavList

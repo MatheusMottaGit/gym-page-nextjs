1845769c466e493d5c523dc8176d24d1bc76cc15
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-14 19:35:04 -0300

    pages

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -2,11 +2,22 @@ import React from 'react'
 import Image from 'next/image'
 
 import barGuy from '../assets/BarGuy.svg'
+import Description from '@/components/AboutUs/Description'
+import Title from '@/components/AboutUs/Title'
 
 const AboutUs = () => {
   return (
     <section className="w-full bg-aboutUs bg-cover bg-center bg-fixed h-screen p-24 flex items-center justify-between gap-5">
-      
+      <div className='flex flex-col items-start gap-8'>
+        <Title />
+
+        <Description />
+
+        <button className='border-[1px] border-red uppercase text-red p-4 font-bebas w-32'>
+          About us
+        </button>
+      </div>
+
     </section>
   )
 }

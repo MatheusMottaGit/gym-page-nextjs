a200def31eeaa4f587db366f0e24956b9438858d
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-15 11:24:00 -0300

    pages

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -3,22 +3,14 @@ import React from 'react'
 import Description from '@/components/AboutUs/Description'
 import Title from '@/components/AboutUs/Title'
 import BarGuy from '@/components/AboutUs/BarGuy'
+import Button from '@/components/Button'
 
 const AboutUs = () => {
   return (
     <section className="w-full bg-aboutUs bg-cover bg-center bg-fixed h-screen p-24 flex items-center justify-between gap-5">
-      <div className='flex flex-col items-start gap-8'>
-        <Title />
-
-        <Description />
-
-        <button className='border-[1px] border-red uppercase text-red p-4 font-bebas w-32'>
-          About us
-        </button>
+      <div>
+        
       </div>
-
-      <BarGuy />
-
     </section>
   )
 }
--- src/pages/CTA.tsx
+++ src/pages/CTA.tsx
@@ -3,19 +3,12 @@ import Image from 'next/image'
 
 import man from '../assets/Man.svg'
 import FormContainer from '@/components/CTA/FormContainer'
-import Form from '@/components/CTA/Form'
 
 const CTA = () => {
   return (
    <section className="w-full bg-chineseblack h-screen p-24 flex items-center justify-center"> 
     <FormContainer>
-      <Image
-        src={man}
-        alt=''
-        className='h-full w-full max-[888px]:invisible'
-      />
-
-      <Form />
+      
     </FormContainer>
    </section>
   )

22802b2ad773f4d43863af88c12573d99250cc5d
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-15 07:45:39 -0300

    main-app-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AboutUs from "@/pages/AboutUs"
 import HomePage from "@/pages/Home"
+import Trainers from "@/pages/Trainers"
 import WhyChooseUs from "@/pages/WhyChooseUs"
 import React from "react"
 
@@ -11,6 +12,8 @@ export default function Home() {
       <WhyChooseUs />
 
       <AboutUs />
+
+      <Trainers />
     </>
   )
 }

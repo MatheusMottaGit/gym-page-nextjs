88ed6016a3a041f1fae3e871b438bbd7c13378c2
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-16 16:17:39 -0300

    pages

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -7,10 +7,18 @@ import Button from '@/components/Button'
 
 const AboutUs = () => {
   return (
-    <section className="w-full bg-aboutUs bg-cover bg-center bg-fixed h-screen p-24 flex items-center justify-between gap-5">
-      <div>
-        
+    <section className="w-full bg-aboutUs bg-cover bg-center bg-fixed h-screen p-24 flex items-center justify-between gap-12 max-[1085px]:flex-col max-[1085px]:h-fit">
+      <div className='flex flex-col gap-5 max-[1085px]:items-center max-[1085px]:text-center z-10'>
+        <Title />
+
+        <Description />
+
+        <Button>
+          Let's join now
+        </Button>
       </div>
+
+      <BarGuy />
     </section>
   )
 }
--- src/pages/CTA.tsx
+++ src/pages/CTA.tsx
@@ -8,7 +8,11 @@ const CTA = () => {
   return (
    <section className="w-full bg-chineseblack h-screen p-24 flex items-center justify-center"> 
     <FormContainer>
-      
+      <Image 
+        src={man} 
+        alt=''
+      />
+
     </FormContainer>
    </section>
   )

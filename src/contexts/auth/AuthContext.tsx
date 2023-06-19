a40c970afe957583b99629fc0dd371a8d7878401
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-19 09:17:34 -0300

    auth-context

--- src/contexts/auth/AuthContext.tsx
+++ src/contexts/auth/AuthContext.tsx
@@ -1,23 +1,51 @@
+'use client'
 import { api } from "@/api/api";
-import { Children } from "@/types/types";
-import { createContext } from "react";
+import { Children, User } from "@/types/types";
+import { Dispatch, SetStateAction, createContext, useEffect, useState } from "react";
+import { cookies } from 'next/headers'
+import { getCookiesData } from "@/lib/getUser";
 
 interface Auth {
  signIn: (email: string, password: string) => void
+ signOut: () => void
+ signed: boolean
+ setUser: Dispatch<SetStateAction<User | null>>
 }
 
 export const AuthContext = createContext<Auth>({} as Auth)
 
 export const AuthProvider = ({ children }: Children) => {
 
+  const [user, setUser] = useState({} as User | null);
+
+  useEffect(() => {
+    getCookiesData()
+  }, [])
+
   async function signIn(email: string, password: string) {
     const response = await api.post('/register', {
-      
+      email,
+      password
     })
+
+    setUser(response.data.user)
+
+    const { token } = response.data
+
+    api.defaults.headers.common[ "Authorization" ] = `Bearer ${token}`
+
+    cookies().set('token', token)
+    cookies().set('user', response.data.user)
+
+  }
+
+  function signOut(){
+    cookies().delete('token')
+    setUser(null)
   }
 
   return (
-    <AuthContext.Provider value={{ signIn }}>
+    <AuthContext.Provider value={{ signIn, signOut, signed: !!user, setUser }}>
       {children}
     </AuthContext.Provider>
   )
